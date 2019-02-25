67474301db92689bb3b572131845a12726419063
Cristina Murillo <cristinamurillo@Cristinas-Air.home>
2019-02-24 21:15:45 -0500

    added seeds for organizations and opportunities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Opportunity.destroy_all
+Organization.destroy_all
+
+20.times do
+    Organization.create(
+        name: Faker::Hipster.word,
+        description: Faker::Hipster.paragraph,
+        icon_url: Faker::LoremFlickr.image("50x60")
+    )
+end
+
+20.times do
+    Opportunity.create(
+        title: Faker::Hipster.sentence(3),
+        description: Faker::Lorem.paragraph,
+        img_url: Faker::LoremFlickr.image,
+        location: "#{Faker::Address.latitude}, #{Faker::Address.longitude}",
+        paid: Faker::Boolean.boolean,
+        organization_id: Faker::Number.between(1, 20) 
+    )
+end
+
+puts "seeded"
