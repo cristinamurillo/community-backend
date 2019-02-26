1fd8fbc8cf78fc2b1c48f15157299e4631884754
Cristina Murillo <cristinamurillo@Cristinas-Air.home>
2019-02-26 09:47:41 -0500

    added user controller, serializer and initial routes. included organization association in user render

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :opportunities, only: [:index, :show, :create, :update]
   resources :organizations, only: [:index, :show, :create, :update]
-
+  resources :users, only: [:show]
 
 end
