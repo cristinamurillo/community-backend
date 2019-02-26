ad5d350ef322d1e05eb9408f1f0b30585b1f8d5a
Cristina Murillo <cristinamurillo@Cristinas-Air.home>
2019-02-25 22:19:37 -0500

    seed file has lat/long coordiantes for new york

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,50 @@ require 'faker'
 Opportunity.destroy_all
 Organization.destroy_all
 
+
 20.times do
     Organization.create(
         name: Faker::Hipster.word,
         description: Faker::Hipster.paragraph,
         icon_url: 'https://pbs.twimg.com/profile_images/971478895005503488/I7ekAuBd_400x400.jpg'
-    )
-end
+        )
+    end
+    
+latitude = 40.730610 
+longitude= -73.95242 
 
-20.times do
+10.times do
+    longitude-=rand()*0.05
+
     Opportunity.create(
         title: Faker::Hipster.sentence(3),
         description: Faker::Lorem.paragraph,
         img_url: Faker::LoremFlickr.image,
-        location: "#{Faker::Address.latitude}, #{Faker::Address.longitude}",
+        location: "#{latitude}, #{longitude}",
         paid: Faker::Boolean.boolean,
         organization_id: Faker::Number.between(1, 20) 
     )
+    latitude+=rand()*0.02
+    longitude= -73.95242 
+
+end
+
+latitude = 40.730610 
+
+
+10.times do
+    longitude+=rand()*0.05
+
+    Opportunity.create(
+        title: Faker::Hipster.sentence(3),
+        description: Faker::Lorem.paragraph,
+        img_url: Faker::LoremFlickr.image,
+        location: "#{latitude}, #{longitude}",
+        paid: Faker::Boolean.boolean,
+        organization_id: Faker::Number.between(1, 20) 
+    )
+    latitude-=rand()*0.02
+    longitude= -73.95242 
 end
 
 puts "seeded"
